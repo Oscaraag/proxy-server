ce6a601375bca0aaa2c150f59d2cddb8d37fa2b3
Oscar <oscaraag1493@gmail.com>
2022-06-14 17:34:36 -0400

    [fix] deployment

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const { createServer } = require('cors-anywhere')
 
 const host = process.env.HOST || '0.0.0.0'
+const path = process.env.path
 const port = process.env.PORT || 8080
 
 createServer({
   originWhitelist: [], // Allow all origins
   requireHeader: ['origin', 'x-requested-with'],
   removeHeaders: ['cookie', 'cookie2'],
-}).listen(port, function () {
+}).listen(port, `${host}${path}`, function () {
   console.log('Running CORS Anywhere on ' + host + ':' + port)
 })
